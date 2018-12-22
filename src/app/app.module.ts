09e4ac0bba9344914088c2b44c2887a3fc101d57
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-22 10:43:19 -0800

    warning component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule
