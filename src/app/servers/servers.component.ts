112f56cd84232674fd355edfea696eca6f0aa879
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-22 16:42:14 -0800

    Two way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -23,8 +23,5 @@ export class ServersComponent implements OnInit {
   onCreateServer() {
     this.serverCreationStatus = 'Server was created!'
   }
-
-  onUpdateServerName(event: any) {
-    this.serverName = (<HTMLInputElement>event.target).value;
-  }
+  
 }
