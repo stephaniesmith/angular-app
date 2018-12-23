1985a92383add3dca3f9d4c631025e0f9b552c83
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-22 16:53:24 -0800

    Using built in directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = '';
+  serverCreated = false;
 
   
   constructor() { 
@@ -21,7 +22,8 @@ export class ServersComponent implements OnInit {
   }
   
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!'
+    this.serverCreated = true;
+    this.serverCreationStatus = `${this.serverName} was created!`
   }
-  
+
 }
